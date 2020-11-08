a63b410456ebf285b8a5bee92a35e2ec2418de66
Neel <neelusenthil1996@gmail.com>
2020-11-08 19:45:45 +0530

    change git changed files api to fetch staged files from worktree stat instead of separate commit iterator loop

--- git/git_changed_files.go
+++ git/git_changed_files.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
-	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/neel1996/gitconvex-server/global"
 	"github.com/neel1996/gitconvex-server/graph/model"
 	"strings"
@@ -14,6 +13,8 @@ import (
 // The function organizes the tracked, untracked and staged files in separate slices and returns the struct *model.GitChangeResults
 
 func ChangedFiles(repo *git.Repository) *model.GitChangeResults {
+	UnPushedCommits(repo)
+
 	var hash plumbing.Hash
 	var stagedFiles []*string
 	var unTrackedFiles []*string
@@ -31,7 +32,6 @@ func ChangedFiles(repo *git.Repository) *model.GitChangeResults {
 
 	logger.Log(fmt.Sprintf("Fetching latest commit object for -> %s", hash), global.StatusInfo)
 
-	commit, commitErr := repo.CommitObject(hash)
 	w, _ := repo.Worktree()
 	stat, _ := w.Status()
 
@@ -40,23 +40,8 @@ func ChangedFiles(repo *git.Repository) *model.GitChangeResults {
 	var statusIndicator string
 	var filePath string
 
-	if commitErr != nil {
-		logger.Log(fmt.Sprintf("Unable to fetch commit at HEAD for %s --> %s", hash, commitErr.Error()), global.StatusError)
-	} else {
-		fileItr, _ := commit.Files()
-
-		_ = fileItr.ForEach(func(file *object.File) error {
-			stagedStat := string(stat.File(file.Name).Staging)
-
-			if stagedStat == "M" || stagedStat == "D" {
-				logger.Log(fmt.Sprintf("Staged entry -> %v", file.Name), global.StatusInfo)
-				stagedFiles = append(stagedFiles, &file.Name)
-			}
-			return nil
-		})
-	}
-
 	for _, statEntry := range statLines {
+		var stagedFile string
 		if len(statEntry) == 0 {
 			continue
 		}
@@ -67,17 +52,10 @@ func ChangedFiles(repo *git.Repository) *model.GitChangeResults {
 			statusIndicator = splitEntry[0]
 			filePath = strings.TrimSpace(strings.Join(splitEntry[1:], " "))
 
-			// Conditional logic to filter out staged entries from modified file list
-			var isStaged bool
-			for _, stagedItem := range stagedFiles {
-				if *stagedItem == filePath {
-					isStaged = true
-					break
-				}
-			}
-
-			if isStaged {
-				continue
+			stagedStat := stat.File(filePath).Staging
+			if string(stagedStat) == "M" {
+				stagedFile = filePath
+				stagedFiles = append(stagedFiles, &stagedFile)
 			}
 
 			switch statusIndicator {
@@ -88,8 +66,10 @@ func ChangedFiles(repo *git.Repository) *model.GitChangeResults {
 				break
 			case "M":
 				logger.Log(fmt.Sprintf("Modified entry - %s -> %v", statusIndicator, filePath), global.StatusInfo)
-				changeStr := "M," + filePath
-				modifiedFiles = append(modifiedFiles, &changeStr)
+				if filePath != stagedFile {
+					changeStr := "M," + filePath
+					modifiedFiles = append(modifiedFiles, &changeStr)
+				}
 				break
 			case "D":
 				logger.Log(fmt.Sprintf("Removed entry - %s -> %v", statusIndicator, filePath), global.StatusInfo)
