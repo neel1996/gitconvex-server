9469a8fb652244b09feb6c4cbae38f8e45ae6559
Neel <neelusenthil1996@gmail.com>
2020-11-09 14:46:47 +0530

    add api to fetch un-pushed commits from the target repo

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -5,7 +5,6 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/neel1996/gitconvex-server/api"
 	"github.com/neel1996/gitconvex-server/git"
@@ -188,13 +187,3 @@ func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *queryResolver) GitUnpushedCommits(ctx context.Context, repoID string, remoteURL string, remoteBranch string) ([]*string, error) {
-	panic(fmt.Errorf("not implemented"))
-}
