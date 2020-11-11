2e3467488e030df7cac5fd078dd85b2eb7ff145e
Neel <neelusenthil1996@gmail.com>
2020-11-11 23:45:29 +0530

    replace go-git repo status function with native git commands to resolve slowness for huge repos

--- git/git_changed_files.go
+++ git/git_changed_files.go
@@ -5,101 +5,119 @@ import (
 	"github.com/go-git/go-git/v5"
 	"github.com/neel1996/gitconvex-server/global"
 	"github.com/neel1996/gitconvex-server/graph/model"
+	"github.com/neel1996/gitconvex-server/utils"
 	"strings"
 )
 
+/*
+	The file invokes the GetGitClient function to run native git commands in the system terminal to fetch some results.
+	The (WorkTree) Status function is slower for huge repos which is taking minutes to return the results, so git client is called
+	to do the job
+*/
+
+// GetUntrackedFiles executes a native git command to fetch the list of untracked files
+func GetUntrackedFiles(repoPath string, untrackedChan chan []*string) {
+	logger.Log("Fetching untracked files from the repo", global.StatusInfo)
+	args := []string{"ls-files", "--others", "--exclude-standard"}
+	cmd := utils.GetGitClient(repoPath, args)
+	cmdString, cmdErr := cmd.Output()
+	if cmdErr != nil {
+		logger.Log(fmt.Sprintf("Command execution failed -> %s", cmdErr.Error()), global.StatusError)
+		untrackedChan <- []*string{}
+	} else {
+		trimStr := strings.TrimSpace(string(cmdString))
+		splitLines := strings.Split(trimStr, "\n")
+		logger.Log(fmt.Sprintf("Untracked files --> %v", splitLines), global.StatusInfo)
+		var fileList []*string
+		for _, file := range splitLines {
+			if file != "" {
+				fileList = append(fileList, &file)
+			}
+		}
+		untrackedChan <- fileList
+	}
+}
+
+// GetStagedFiles executes a native git command to fetch the list of staged files
+func GetStagedFiles(repoPath string, stagedFileChan chan []*string) {
+	logger.Log("Fetching staged files from the repo", global.StatusInfo)
+	args := []string{"diff", "--name-only", "--cached"}
+	cmd := utils.GetGitClient(repoPath, args)
+	cmdString, cmdErr := cmd.Output()
+	if cmdErr != nil {
+		logger.Log(fmt.Sprintf("Command execution failed -> %s", cmdErr.Error()), global.StatusError)
+		stagedFileChan <- []*string{}
+	} else {
+		trimStr := strings.TrimSpace(string(cmdString))
+		splitLines := strings.Split(trimStr, "\n")
+		logger.Log(fmt.Sprintf("Staged files --> %v", splitLines), global.StatusInfo)
+
+		var fileList []*string
+		for _, file := range splitLines {
+			if file != "" {
+				temp := file
+				fileList = append(fileList, &temp)
+			}
+		}
+		stagedFileChan <- fileList
+	}
+}
+
+// GetModifiedFiles executes a native git command to fetch all the modified files from the repo
+func GetModifiedFiles(repoPath string, modifiedFileChan chan []*string) {
+	logger.Log("Fetching changed files from the repo", global.StatusInfo)
+	args := []string{"diff", "--raw"}
+	cmd := utils.GetGitClient(repoPath, args)
+	cmdString, cmdErr := cmd.Output()
+	if cmdErr != nil && cmdString != nil {
+		logger.Log(fmt.Sprintf("Command execution failed -> %s", cmdErr.Error()), global.StatusError)
+		modifiedFileChan <- []*string{}
+	} else {
+		trimStr := strings.TrimSpace(string(cmdString))
+		splitLines := strings.Split(trimStr, "\n")
+		var fileList []*string
+		for _, change := range splitLines {
+			if change == "" {
+				continue
+			}
+			fileName := strings.Fields(change)
+			logger.Log(fmt.Sprintf("Changed file --> %v", fileName), global.StatusInfo)
+			changeStr := "M," + fileName[5]
+			fileList = append(fileList, &changeStr)
+		}
+		modifiedFileChan <- fileList
+	}
+}
+
 // ChangedFiles returns the list of changes from the target
 // The function organizes the tracked, untracked and staged files in separate slices and returns the struct *model.GitChangeResults
-
 func ChangedFiles(repo *git.Repository) *model.GitChangeResults {
-	var stagedFiles []*string
-	var unTrackedFiles []*string
-	var modifiedFiles []*string
-	var newStagedItems []string
-
 	logger := global.Logger{}
 
 	logger.Log("Fetching the current status of the repo", global.StatusInfo)
 	w, _ := repo.Worktree()
-	stat, _ := w.Status()
+	repoPath := w.Filesystem.Root()
 
-	statLines := strings.Split(stat.String(), "\n")
+	var unTrackedFileChan = make(chan []*string)
+	var changedFileChan = make(chan []*string)
+	var stagedFileChan = make(chan []*string)
 
-	var statusIndicator string
-	var filePath string
+	go GetModifiedFiles(repoPath, changedFileChan)
+	go GetUntrackedFiles(repoPath, unTrackedFileChan)
+	go GetStagedFiles(repoPath, stagedFileChan)
 
-	for _, statEntry := range statLines {
-		var stagedFile string
-		if len(statEntry) == 0 {
-			continue
-		}
-		statEntry := strings.TrimSpace(statEntry)
-
-		if strings.Contains(statEntry, " ") {
-			splitEntry := strings.Split(statEntry, " ")
-			statusIndicator = splitEntry[0]
-			filePath = strings.TrimSpace(strings.Join(splitEntry[1:], " "))
-
-			stagedStat := stat.File(filePath).Staging
-			if string(stagedStat) == "M" {
-				stagedFile = filePath
-				stagedFiles = append(stagedFiles, &stagedFile)
-			}
-
-			switch statusIndicator {
-			case "?", "??":
-				logger.Log(fmt.Sprintf("Untracked entry -> %v", filePath), global.StatusInfo)
-				changeStr := filePath
-				unTrackedFiles = append(unTrackedFiles, &changeStr)
-				break
-			case "M":
-				logger.Log(fmt.Sprintf("Modified entry - %s -> %v", statusIndicator, filePath), global.StatusInfo)
-				if filePath != stagedFile {
-					changeStr := "M," + filePath
-					modifiedFiles = append(modifiedFiles, &changeStr)
-				}
-				break
-			case "D":
-				logger.Log(fmt.Sprintf("Removed entry - %s -> %v", statusIndicator, filePath), global.StatusInfo)
-				changeStr := "D," + filePath
-				modifiedFiles = append(modifiedFiles, &changeStr)
-				break
-			case "A":
-				logger.Log(fmt.Sprintf("New Staged entry - %s -> %v", statusIndicator, filePath), global.StatusInfo)
-				newStagedItems = append(newStagedItems, filePath)
-				break
-			case "AM":
-				logger.Log(fmt.Sprintf("New Staged entry - %s -> %v", statusIndicator, filePath), global.StatusInfo)
-				newStagedItems = append(newStagedItems, filePath)
-				changeStr := "M," + filePath
-				modifiedFiles = append(modifiedFiles, &changeStr)
-				break
-			}
-		} else {
-			logger.Log(fmt.Sprintf("Status indicator cannot be obtained for -> %s", statEntry), global.StatusError)
-			break
-		}
+	// Intermediate return value to close the channels and then return the values
+	returnVal := &model.GitChangeResults{
+		GitUntrackedFiles: <-unTrackedFileChan,
+		GitChangedFiles:   <-changedFileChan,
+		GitStagedFiles:    <-stagedFileChan,
 	}
 
-	// Loop to iterate and append untracked and deleted staged files to staged item list
-	for _, entry := range newStagedItems {
-		stagedFiles = append(stagedFiles, &entry)
-	}
+	defer func() {
+		close(stagedFileChan)
+		close(changedFileChan)
+		close(unTrackedFileChan)
+	}()
 
-	// Conditional logic to remove duplicate items from staged files list
-	var refMap = make(map[string]bool)
-	var refinedStagedFiles []*string
-
-	for _, entry := range stagedFiles {
-		if mapEntry := refMap[*entry]; !mapEntry {
-			refMap[*entry] = true
-			refinedStagedFiles = append(refinedStagedFiles, entry)
-		}
-	}
-
-	return &model.GitChangeResults{
-		GitUntrackedFiles: unTrackedFiles,
-		GitChangedFiles:   modifiedFiles,
-		GitStagedFiles:    refinedStagedFiles,
-	}
+	return returnVal
 }
