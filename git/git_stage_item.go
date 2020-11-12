5c840323526b607f91b0d2be4ab95c61cc03a2c1
Neel <neelusenthil1996@gmail.com>
2020-11-12 12:47:41 +0530

    replace go-git based module for stage and stage all operations with native git command to resolve slowness for large repos

--- git/git_stage_item.go
+++ git/git_stage_item.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/go-git/go-git/v5"
 	"github.com/neel1996/gitconvex-server/global"
+	"github.com/neel1996/gitconvex-server/utils"
 )
 
 func addError(fileItem string, errMsg string) string {
@@ -12,19 +13,23 @@ func addError(fileItem string, errMsg string) string {
 	return "ADD_ITEM_FAILED"
 }
 
+// StageItem stages a selected file from the target repo
+// The function relies on the native git client to stage an item, as go-git staging is time consuming for huge repos
 func StageItem(repo *git.Repository, fileItem string) string {
 	logger := global.Logger{}
 	w, wErr := repo.Worktree()
 	if wErr != nil {
 		return addError(fileItem, wErr.Error())
 	} else {
-		itemHash, addErr := w.Add(fileItem)
-		if addErr != nil {
-			return addError(fileItem, addErr.Error())
+		args := []string{"add", fileItem}
+		cmd := utils.GetGitClient(w.Filesystem.Root(), args)
+		cmdString, cmdErr := cmd.Output()
+		if cmdErr != nil {
+			logger.Log(fmt.Sprintf("Staging of %s failed -> %s", fileItem, cmdErr.Error()), global.StatusError)
+			return addError(fileItem, cmdErr.Error())
 		} else {
-			logger.Log(fmt.Sprintf("New item -> %s added to the repo worktree --> %s", fileItem, itemHash.String()), global.StatusInfo)
+			logger.Log(fmt.Sprintf("File -> %s staged \n%s", fileItem, cmdString), global.StatusInfo)
 			return "ADD_ITEM_SUCCESS"
 		}
 	}
-
 }
--- git/git_stageall_items.go
+++ git/git_stageall_items.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"github.com/go-git/go-git/v5"
 	"github.com/neel1996/gitconvex-server/global"
-	"strings"
+	"github.com/neel1996/gitconvex-server/utils"
 )
 
+// StageAllItems stages all the modified and untracked items to the worktree
+// The function relies on the native git client to stage an item, as go-git staging is time consuming for huge repos
 func StageAllItems(repo *git.Repository) string {
-	var stageCounter int
-	stageCounter = 0
 	w, wErr := repo.Worktree()
 	logger.Log(fmt.Sprintf("Staging all changes from repo"), global.StatusInfo)
 
@@ -17,45 +17,15 @@ func StageAllItems(repo *git.Repository) string {
 		logger.Log(fmt.Sprintf("Error occurred while fetching worktree -> %s", wErr.Error()), global.StatusError)
 		return "ALL_STAGE_FAILED"
 	} else {
-		status, statErr := w.Status()
-		if statErr != nil {
-			logger.Log(fmt.Sprintf("Error occurred while fetching repo status -> %s", statErr.Error()), global.StatusError)
+		args := []string{"add", "--all"}
+		cmd := utils.GetGitClient(w.Filesystem.Root(), args)
+		cmdStr, cmdErr := cmd.Output()
+		if cmdErr != nil {
+			logger.Log(fmt.Sprintf("Error occurred while fetching repo status -> %s", cmdErr.Error()), global.StatusError)
+			return "ALL_STAGE_FAILED"
 		} else {
-			statLines := strings.Split(status.String(), "\n")
-
-			for _, statEntry := range statLines {
-				if len(statEntry) == 0 {
-					continue
-				}
-				statEntry := strings.TrimSpace(statEntry)
-				if strings.Contains(statEntry, " ") {
-					splitEntry := strings.Split(statEntry, " ")
-					statusIndicator := splitEntry[0]
-					fileItem := strings.TrimSpace(strings.Join(splitEntry[1:], " "))
-
-					if statusIndicator == "A" {
-						continue
-					}
-
-					itemHash, addErr := w.Add(fileItem)
-					if addErr != nil {
-						logger.Log(fmt.Sprintf("Error occurred while staging -> %s --> %s", fileItem, addErr.Error()), global.StatusError)
-					} else {
-						stageCounter++
-						logger.Log(fmt.Sprintf("New item -> %s added to the repo worktree --> %s", fileItem, itemHash.String()), global.StatusInfo)
-					}
-
-				} else {
-					logger.Log(fmt.Sprintf("Status indicator cannot be obtained for -> %s", statEntry), global.StatusError)
-					break
-				}
-			}
+			logger.Log(fmt.Sprintf("All changes staged -> %s", cmdStr), global.StatusInfo)
+			return "ALL_STAGED"
 		}
-	}
-
-	if stageCounter > 0 {
-		return "ALL_STAGED"
-	} else {
-		return "ALL_STAGE_FAILED"
 	}
 }
