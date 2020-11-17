1cfa31e03a4c73d717cdef00566ccc6a0e46f2f1
Neel <neelusenthil1996@gmail.com>
2020-11-18 00:47:34 +0530

    add fixed route to supply favicon image to be displayed in the web page

--- server.go
+++ server.go
@@ -63,6 +63,7 @@ func main() {
 
 	// Static file supplier for hosting the react static assets and scripts
 	router.PathPrefix("/static").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("./build/static/"))))
+	router.PathPrefix("/gitconvex.png").Handler(http.StripPrefix("/gitconvex.png", http.FileServer(http.Dir("./build/gitconvex.png"))))
 
 	// A default fallback route for handling all routes with '/' prefix.
 	// For making it compatible with react router
