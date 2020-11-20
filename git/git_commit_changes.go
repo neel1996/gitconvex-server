2d1ac84e0018e6f76f461a324b649f1da8cb824e
Neel <neelusenthil1996@gmail.com>
2020-11-20 21:38:13 +0530

    fix for commit change api to include proper commit message and add logic to fail if message is empty

--- git/git_commit_changes.go
+++ git/git_commit_changes.go
@@ -3,14 +3,10 @@ package git
 import (
 	"fmt"
 	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/config"
-	"github.com/go-git/go-git/v5/plumbing"
-	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/neel1996/gitconvex-server/global"
 	"github.com/neel1996/gitconvex-server/utils"
 	"runtime"
 	"strings"
-	"time"
 )
 
 // windowsCommit is used for committing changes using the git client if the platform is windows
@@ -34,7 +30,7 @@ func windowsCommit(repoPath string, msg string) string {
 // The function falls back to the native git client for Windows platform due to an existing bug in the go-git library which
 // blocks commits in windows platform
 func CommitChanges(repo *git.Repository, commitMessage string) string {
-	var formattedMessage string
+	var formattedMessage = commitMessage
 	logger := global.Logger{}
 	w, wErr := repo.Worktree()
 
@@ -46,8 +42,6 @@ func CommitChanges(repo *git.Repository, commitMessage string) string {
 		if strings.Contains(commitMessage, "||") {
 			splitMessage := strings.Split(commitMessage, "||")
 			formattedMessage = strings.Join(splitMessage, "\n")
-		} else {
-			formattedMessage = commitMessage
 		}
 
 		// Checking OS platform for switching to git client for Windows systems
@@ -65,61 +59,64 @@ func CommitChanges(repo *git.Repository, commitMessage string) string {
 			return windowsCommit(w.Filesystem.Root(), formattedMessage)
 		}
 
+		return "c"
+
 		// Logic to check if the repo / global config has proper user information setup
 		// Commit will be signed by default user if no user config is present
-		globalConfig, gCfgErr := repo.ConfigScoped(config.GlobalScope)
-		localConfig, lCfgErr := repo.ConfigScoped(config.LocalScope)
-		var author string
 
-		if gCfgErr == nil && lCfgErr == nil {
-			fmt.Println(localConfig.User)
-			fmt.Println(globalConfig.User)
-
-			if globalConfig.User.Name != "" {
-				author = globalConfig.User.Name
-			} else if localConfig.User.Name != "" {
-				author = localConfig.User.Name
-			}
-		} else {
-			logger.Log(fmt.Sprintf("Unable to fetch repo config -> %v || %v", gCfgErr, lCfgErr), global.StatusError)
-			return "COMMIT_FAILED"
-		}
-
-		var commitOptions *git.CommitOptions
-		var parentHash plumbing.Hash
-		head, headErr := repo.Head()
-		if headErr != nil {
-			logger.Log(headErr.Error(), global.StatusError)
-		} else {
-			parentHash = head.Hash()
-		}
-
-		if author == "" {
-			logger.Log(fmt.Sprintf("No author name is available for the repo.\nSetting default signature for the commit"), global.StatusWarning)
-			logger.Log("You can set the author details using git config commands --> https://support.atlassian.com/bitbucket-cloud/docs/configure-your-dvcs-username-for-commits/", global.StatusWarning)
-			commitOptions = &git.CommitOptions{
-				All: false,
-				Author: &object.Signature{
-					Name:  "gitconvex",
-					Email: "help@gitconvex.com",
-					When:  time.Now(),
-				},
-				Parents: []plumbing.Hash{parentHash},
-			}
-		} else {
-			logger.Log(fmt.Sprintf("Commiting changes with author -> %s", author), global.StatusInfo)
-			commitOptions = &git.CommitOptions{
-				All:     false,
-				Parents: []plumbing.Hash{parentHash},
-			}
-		}
-		hash, err := w.Commit(formattedMessage, commitOptions)
-		if err != nil {
-			logger.Log(fmt.Sprintf("Error occurred while committing changes -> %s\n%v", err.Error(), err), global.StatusError)
-			return "COMMIT_FAILED"
-		} else {
-			logger.Log(fmt.Sprintf("Staged changes have been comitted - %s", hash.String()), global.StatusInfo)
-			return "COMMIT_DONE"
-		}
+		//globalConfig, gCfgErr := repo.ConfigScoped(config.GlobalScope)
+		//localConfig, lCfgErr := repo.ConfigScoped(config.LocalScope)
+		//var author string
+		//
+		//if gCfgErr == nil && lCfgErr == nil {
+		//	fmt.Println(localConfig.User)
+		//	fmt.Println(globalConfig.User)
+		//
+		//	if globalConfig.User.Name != "" {
+		//		author = globalConfig.User.Name
+		//	} else if localConfig.User.Name != "" {
+		//		author = localConfig.User.Name
+		//	}
+		//} else {
+		//	logger.Log(fmt.Sprintf("Unable to fetch repo config -> %v || %v", gCfgErr, lCfgErr), global.StatusError)
+		//	return "COMMIT_FAILED"
+		//}
+		//
+		//var commitOptions *git.CommitOptions
+		//var parentHash plumbing.Hash
+		//head, headErr := repo.Head()
+		//if headErr != nil {
+		//	logger.Log(headErr.Error(), global.StatusError)
+		//} else {
+		//	parentHash = head.Hash()
+		//}
+		//
+		//if author == "" {
+		//	logger.Log(fmt.Sprintf("No author name is available for the repo.\nSetting default signature for the commit"), global.StatusWarning)
+		//	logger.Log("You can set the author details using git config commands --> https://support.atlassian.com/bitbucket-cloud/docs/configure-your-dvcs-username-for-commits/", global.StatusWarning)
+		//	commitOptions = &git.CommitOptions{
+		//		All: false,
+		//		Author: &object.Signature{
+		//			Name:  "gitconvex",
+		//			Email: "help@gitconvex.com",
+		//			When:  time.Now(),
+		//		},
+		//		Parents: []plumbing.Hash{parentHash},
+		//	}
+		//} else {
+		//	logger.Log(fmt.Sprintf("Commiting changes with author -> %s, message -> %s", author, formattedMessage), global.StatusInfo)
+		//	commitOptions = &git.CommitOptions{
+		//		All:     false,
+		//		Parents: []plumbing.Hash{parentHash},
+		//	}
+		//}
+		//hash, err := w.Commit(formattedMessage, commitOptions)
+		//if err != nil {
+		//	logger.Log(fmt.Sprintf("Error occurred while committing changes -> %s\n%v", err.Error(), err), global.StatusError)
+		//	return "COMMIT_FAILED"
+		//} else {
+		//	logger.Log(fmt.Sprintf("Staged changes have been comitted - %s", hash.String()), global.StatusInfo)
+		//	return "COMMIT_DONE"
+		//}
 	}
 }
