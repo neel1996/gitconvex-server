30f71120a139f106251e49c59286ca1a95a7f7a8
neel1996 <neelusenthil1996@gmail.com>
2021-07-12 23:52:39 +0530

    add common package interface for commit

--- utils/array_generator.go
+++ utils/array_generator.go
@@ -9,3 +9,13 @@ func GeneratePointerArrayFrom(array []string) []*string {
 
 	return pointerArray
 }
+
+func GenerateNonPointerArrayFrom(array []*string) []string {
+	var pointerArray []string
+
+	for _, element := range array {
+		pointerArray = append(pointerArray, *element)
+	}
+
+	return pointerArray
+}
