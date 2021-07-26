4ea944bc78d6286bf32df5f1e3f44ca3dc949877
neel1996 <neelusenthil1996@gmail.com>
2021-07-26 23:32:07 +0530

    create new mapper to bind commit log api responses to the combined model

--- generate_all_mocks.sh
+++ generate_all_mocks.sh
@@ -6,3 +6,4 @@ mockgen -source=git/middleware/reference.go -destination=mocks/mock_reference.go
 mockgen -source=git/middleware/index.go -destination=mocks/mock_index.go -package=mocks
 mockgen -source=git/commit/git_list_all_commit_logs.go -destination=mocks/mock_git_list_all_commit_logs.go -package=mocks
 mockgen -source=git/middleware/commit.go -destination=mocks/mock_commit.go -package=mocks
+mockgen -source=git/commit/git_commit_file_history.go -destination=mocks/mock_git_commit_file_history.go -package=mocks
